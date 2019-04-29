a77c9481647b7541078ca800ed33512219b0f0e0
Donprecious <iyeritufu@gmail.com>
2019-04-29 12:58:07 +0100

    Initial Commit

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var customer = new CustomerVm().CustomerList();
+
+            return Ok(customer);
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var customer = new CustomerVm().Get(id);
+            if (customer == null) return NotFound("Customer not found");
+            return Ok(customer);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+    }
+
+    public class CustomerVm
+    {
+        IList<Customer> customers = new List<Customer>()
+        {
+            new Customer()
+            {
+                Id = 1,
+                Address = "Los Angelas",
+                FirstName = "jacobs",
+                LastName = "Turesa",
+                Gender = "male",
+                PhoneNumber = "+234802331209"
+            },
+            new Customer()
+            {
+                Id = 2,
+                Address = "Las vegas",
+                FirstName = "Micheal",
+                LastName = "Chiris",
+                Gender = "male",
+                PhoneNumber = "+23480433190"
+            },
+            new Customer()
+            {
+                Id = 3,
+                Address = "lagos Angelas",
+                FirstName = "Fredrick",
+                LastName = "Onos",
+                Gender = "male",
+                PhoneNumber = "+234804331493"
+            },
+            new Customer()
+            {
+                Id = 4,
+                Address = "Moles Joud",
+                FirstName = "Mary",
+                LastName = "Turesa",
+                Gender = "female",
+                PhoneNumber = "+234904331209"
+            },
+
+            new Customer()
+            {
+                Id = 5,
+                Address = "Angela Vegal",
+                FirstName = "Juliet",
+                LastName = "Turesa",
+                Gender = "female",
+                PhoneNumber = "+234234331209"
+            },
+        };
+        public IList<Customer> CustomerList()
+        {
+            return customers;
+        }
+
+        public Customer Get(int id)
+        {
+            var customer = customers.Where(a => a.Id == id).SingleOrDefault();
+            return customer;
+        }
+
+    }
+
+    
+}
